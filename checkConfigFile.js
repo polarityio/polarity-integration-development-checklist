b8521ea5f41b5c026d25072f0322ab2207eb5df9
Jon Penwood <jonp@polarity.io>
2023-03-31 13:07:55 -0500

    fix: missed files adding

--- checkConfigFile.js
+++ checkConfigFile.js
@@ -130,7 +130,7 @@ const checkPolarityIntegrationUuid = async (configJson, octokit, repo) => {
   }
   const token = core.getInput('GITHUB_TOKEN');
   octokit = github.getOctokit(token);
-  const repo = get("context.payload.repository", github);
+  repo = get("context.payload.repository", github);
   const toMergeIntoBranch = github.context.payload.pull_request.base.ref
   const previousCommits = await getExistingFile(octokit, 'polarityio', repo.name, toMergeIntoBranch, 'config/config.json')
   console.info({toMergeIntoBranch,previousCommits})
