23df99a1df2632c411462dfa4ad4ff665c899dfd
Ed Dorsey <ed@polarity.io>
2026-04-15 02:17:10 -0400

    Remove node version

--- checkTestScript.js
+++ checkTestScript.js
@@ -12,7 +12,7 @@ const checkTestScript = () => {
       return;
     }
 
-    execWithNodeVersion(24, "node --version && npm run test");
+    execWithNodeVersion(24, "npm run test");
 
     console.info("- Success: `npm run test` passed with no errors");
   } catch (e) {
